0b1bbdcb82801cc6c2cef13082e4cc9038f0b249
Jake Soenneker <jake@soenneker.com>
2025-10-29 08:14:24 -0500

    interface mod

--- src/Abstract/ICosmosContainerSetupUtil.cs
+++ src/Abstract/ICosmosContainerSetupUtil.cs
@@ -9,9 +9,9 @@ namespace Soenneker.Cosmos.Container.Setup.Abstract;
 /// </summary>
 public interface ICosmosContainerSetupUtil
 {
-    ValueTask<ContainerResponse?> Ensure(string name, CancellationToken cancellationToken = default);
+    ValueTask<ContainerResponse?> Ensure(string containerName, CancellationToken cancellationToken = default);
 
-    ValueTask<ContainerResponse?> Ensure(string name, string containerName, CancellationToken cancellationToken = default);
+    ValueTask<ContainerResponse?> Ensure(string databaseName, string containerName, CancellationToken cancellationToken = default);
 
     ValueTask<ContainerResponse?> Ensure(Microsoft.Azure.Cosmos.Database database, string containerName, CancellationToken cancellationToken = default);
 }
